8087977aa91385f4e176e9fb6254b7ee63cf3c84
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-30 13:56:34 -0400

    BEST THING EVER

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,59 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	var boo = $("#boo");
+	var boo2 = $("#boo2");
+	$('#boo3').hide();
+
+		// basic onload animations
+		
+		TweenLite.to(boo, 100, {
+			left:"3000px"
+		});
+
+		TweenLite.to(boo2, 9, {
+			left:"-200px",
+			opacity:0,
+			ease:SlowMo.ease.config(0.3, 0.6, true)
+		});
+
+		TweenLite.to(boo3, 110, {
+			left:"5000px"
+		});
+
+
+		// click animations
+
+		$('#boo').click(function(){
+			$('#boo3').fadeIn("slow");
+			TweenLite.to(boo, 1.6,{
+				top:'+=40px', 
+				ease:Strong.easeOut
+			});
+		});
+
+		$('#boo3').click(function(){
+			$('#boo').fadeOut("slow");
+			TweenLite.to(boo3, 1.6,{
+				top:'-=40px', 
+				ease:Strong.easeOut
+			});
+		});
+
+		$('#boo2').click(function(){
+			TweenLite.to(boo2, 1.6,{
+				top:'+=40px', 
+				ease:Strong.easeOut
+
+			});
+			TweenLite.to(boo2, 15, {
+				left:"-200px",
+				opacity:0,
+				ease:SlowMo.ease.config(0.3, 0.6, true)
+			});
+		});
+
+		
+	});
