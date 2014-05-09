f852a5c28f69ebbac794fe6c1eedae50d7c10f28
Ilia <ilia@ilia-Z68P-DS3.(none)>
2014-05-08 22:49:59 -0400

    dropped tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509023530) do
-
-  create_table "projects", force: true do |t|
-    t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140509024930) do
 
 end
